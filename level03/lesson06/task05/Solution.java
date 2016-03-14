3b3321da1215b207b73635e29242a65631255c6a
alex29z <alex29z@rambler.ru>
2016-03-14 11:57:48 +0300

    1

---
+++ level03/lesson06/task05/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level03.lesson06.task05;
+
+/* Изучаем японский
+Выведи на экран 日本語
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        char symbol1 = '\u65E5', symbol2 = '\u672C', symbol3 = '\u8A9E';
+        System.out.print(symbol1);
+        System.out.print(symbol2);
+        System.out.print(symbol3);
+    }
+}
